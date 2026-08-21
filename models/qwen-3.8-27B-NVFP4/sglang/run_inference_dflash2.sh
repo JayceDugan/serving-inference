8a8c0099493fd32ed4852d488fd6d4d84d189b7e
Jayce Dugan <jgtdpersonal@gmail.com>
2026-08-21 12:08:56 +1000

    feat: Initial dflash2 config

--- models/qwen-3.8-27B-NVFP4/sglang/run_inference_dflash2.sh
+++ models/qwen-3.8-27B-NVFP4/sglang/run_inference_dflash2.sh
@@ -8,14 +8,22 @@ docker run --rm --gpus '"device=0"' \
   -v ~/.cache/huggingface:/root/.cache/huggingface \
   --env "HF_TOKEN=${HF_TOKEN}" \
   --ipc=host \
-  lmsysorg/sglang:qwen38-27b \
+  lmsysorg/sglang:dev-cu13 \
   sglang serve \
     --trust-remote-code \
     --model-path RadixArk/Qwen3.8-27B-NVFP4 \
-    --mem-fraction-static 0.95 \
+    --kv-cache-dtype fp8_e4m3 \
+    --mem-fraction-static 0.945 \
     --attention-backend flashinfer \
-    --chunked-prefill-size 2048 \
+    --max-running-requests 1 \
+    --cuda-graph-max-bs 1 \
     --reasoning-parser qwen3 \
     --tool-call-parser qwen3_coder \
+    --mamba-full-memory-ratio 10 \
     --host 0.0.0.0 \
-    --port 30000
+    --port 30000 \
+    --speculative-algorithm DFLASH \
+    --speculative-draft-model-path incoai/Qwen3.8-27B-DFlash2 \
+    --speculative-num-draft-tokens 8 \
+    --mamba-radix-cache-strategy extra_buffer_lazy \
+    --mamba-ssm-dtype float32
